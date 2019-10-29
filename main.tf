af9676f7fafd8fc2e3667d0cd192e22420b6c49f
officer <kasaiso@amazon.com>
2019-10-29 17:53:56 +0000

    fix

--- main.tf
+++ main.tf
@@ -5,16 +5,26 @@ resource "aws_cloudfront_distribution" "distribution" {
       acm_certificate_arn = "${var.certificate_arn}"
       ssl_support_method  = "vip"
     }
-    aliases             = "${var.domain_name}"
+    aliases             = "${var.alias_domain_names}"
     is_ipv6_enabled     = true
     
 
-    origin = "${var.origin}"
+    origin {
+      domain_name = "${var.origin_domain_name}"
+      origin_id   = "primary"
+
+      custom_origin_config = {
+        http_port = 80
+        https_port = 443
+        origin_protocol_policy = "http-only"
+        origin_ssl_protocols = "TLSv1.2"
+      }
+    }
 
     default_cache_behavior {
       allowed_methods   = ["GET", "HEAD", "OPTIONS"]
       cached_methods     = ["GET", "HEAD", "OPTIONS"]
-      target_origin_id  = "${var.origin.origin_id}"
+      target_origin_id  = "primary"
 
       viewer_protocol_policy = "redirect-to-https"
       min_ttl                = 0
@@ -46,6 +56,6 @@ resource "aws_cloudfront_distribution" "distribution" {
 module "logging_bucket" {
   source    = "github.com/officer/terraform-logging-bucket.git"
   region    = "${var.s3_region}"
-  namespace = "${var.namespace}"
+  namespace = "cflog"
   tags      = "${var.tags}"
 }
--- variables.tf
+++ variables.tf
@@ -3,12 +3,27 @@ variable "origin" {
   type          = "map"
 }
 
-variable "domain_name" {
-  description   = "(Required) origin domain names"
+variable "alias_domain_names" {
+  description   = "(Required) alias domain names"
   type          = "list"
 }
+
+variable "origin_domain_name" {
+  description   = "(Required) origin domain names"
+  type          = "string"
+}
+
 
 variable "certificate_arn" {
   description = "(Required) ACM Certificate ARN"
   type = "string"
 }
+
+variable "tags" {
+  description = "(Optional) Tags for resource"
+  type        = "map"
+  default = {
+    ENV = "TEST"
+  }
+}
+
